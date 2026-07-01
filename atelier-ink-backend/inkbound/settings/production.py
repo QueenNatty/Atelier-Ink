735849f0b625d6b1cafac0e959b0e81a17a466a0
QueenNatty <nattyeze71@gmail.com>
2026-07-01 08:21:35 +0100

    Parse single production DATABASE_URL string and updated the requirements.txt file for hosting.

--- atelier-ink-backend/inkbound/settings/production.py
+++ atelier-ink-backend/inkbound/settings/production.py
@@ -1,6 +1,7 @@
 from .base import *  # noqa
 from decouple import config, Csv
 import os
+import dj_database_url
 
 DEBUG = False
 
@@ -8,15 +9,11 @@ ALLOWED_HOSTS = config("ALLOWED_HOSTS", cast=Csv())
 ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS", "").split(",")
 
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.postgresql",
-        "NAME": config("DB_NAME"),
-        "USER": config("DB_USER"),
-        "PASSWORD": config("DB_PASSWORD"),
-        "HOST": config("DB_HOST", default="localhost"),
-        "PORT": config("DB_PORT", default="5432"),
-        "CONN_MAX_AGE": 60,
-    }
+    "default": dj_database_url.config(
+        default=os.environ.get('DATABASE_URL'),
+        conn_max_age=60,
+        ssl_require=True
+    )
 }
 
 CORS_ALLOWED_ORIGINS = config("CORS_ALLOWED_ORIGINS", cast=Csv())
